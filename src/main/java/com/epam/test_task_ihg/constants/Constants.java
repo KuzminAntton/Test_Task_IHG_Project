7148fab3272fad776a97a63387ad48cdf988e74c
anton.kuzmin <anton_kuzmin@epam.com>
2019-04-16 14:27:40 +0300

    create Constants class to save all strings in one place

---
+++ src/main/java/com/epam/test_task_ihg/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.epam.test_task_ihg.constants;
+
+public class Constants {
+    public final static String PROPERTY_PATH = "/home/anton/Anton/Test_IHG_Project/src/main/resources/hotelsList.properties";
+    public final static String PROPERTY_NAME_PATH = "path";
+    public final static String HOTELS_THAT_I_COULD_FOUND = "Hotels that I could found.";
+    public final static String ALL_GROUPS_THAT_I_COULD_FOUND = "All groups that I could found.";
+    public final static String HOTELS_THAT_I_COULD_FOUND_IN_THREE_PRICE_RANGES = "Hotels that I could found in three price ranges.";
+    public final static String BEST_REGARDS_AND_HAVE_I_NICE_DAY = "Best regrds and have I nice day.";
+    public final static String REQUEST_IS_NOT_CORRECT = "Request is not correct";
+    public final static String COMMAND_IS_NOT_CORRECT = "Command is not correct";
+    public final static String PARAM_DELIMETER = "-";
+    public final static String EXIT_MESSAGE = "exit-";
+}
