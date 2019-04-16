c0193cd3984efa838439686e82aaa2a101490bfd
anton.kuzmin <anton_kuzmin@epam.com>
2019-04-16 14:29:55 +0300

    custom Exception for controller

---
+++ src/main/java/com/epam/test_task_ihg/controller/exception/ControllerException.java
@@ -0,0 +1,21 @@
+package com.epam.test_task_ihg.controller.exception;
+
+public class ControllerException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public ControllerException() {
+        super();
+    }
+
+    public ControllerException(String message) {
+        super(message);
+    }
+
+    public ControllerException(Exception e) {
+        super(e);
+    }
+
+    public ControllerException(String message, Exception e) {
+        super(message, e);
+    }
+}
