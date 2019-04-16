afd2b69234e189ca50781546c18927d2b44b7ac8
anton.kuzmin <anton_kuzmin@epam.com>
2019-04-16 14:32:07 +0300

    class for save list of Hotels

---
+++ src/main/java/com/epam/test_task_ihg/store/HotelsStore.java
@@ -0,0 +1,37 @@
+package com.epam.test_task_ihg.store;
+
+import com.epam.test_task_ihg.bean.Hotel;
+import com.epam.test_task_ihg.constants.Constants;
+import com.epam.test_task_ihg.store.exception.HotelsStoreException;
+import com.epam.test_task_ihg.util.HotelParserTxt;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+public class HotelsStore {
+    private List<Hotel> hotelsList;
+
+    public void init() throws HotelsStoreException {
+        try {
+             InputStream input = new FileInputStream(Constants.PROPERTY_PATH);
+
+                Properties prop = new Properties();
+
+
+                prop.load(input);
+
+
+            hotelsList = HotelParserTxt.parse(new File(prop.getProperty(Constants.PROPERTY_NAME_PATH)));
+        } catch (IOException e) {
+            throw new HotelsStoreException();
+        }
+    }
+
+    public List<Hotel> getHotelsList() {
+        return hotelsList;
+    }
+}
