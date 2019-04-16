29220df93576252f24484b2b1e4f0c187d4bf2ae
anton.kuzmin <anton_kuzmin@epam.com>
2019-04-16 14:29:06 +0300

    enum for commands names

---
+++ src/main/java/com/epam/test_task_ihg/controller/commands/CommandName.java
@@ -0,0 +1,11 @@
+package com.epam.test_task_ihg.controller.commands;
+
+public enum CommandName {
+
+    GET_FIRST_FIVE_HOTELS_BY_RATE,
+    GET_HOTELS_BY_GROUP,
+    GET_HOTELS_BY_PRICE_RANGE,
+    WRONG_REQUEST,
+    WRONG_COMMAND,
+    EXIT
+}
