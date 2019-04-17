de19f8d9d88cf04a20f8a5ad896b7f61e20f97d8
KuzminAntton <AntonAnton1488>
2019-04-17 14:06:31 +0300

    changed parsing from txt file to json in HotelStore init method

--- src/main/java/com/epam/test_task_ihg/store/HotelsStore.java
+++ src/main/java/com/epam/test_task_ihg/store/HotelsStore.java
@@ -4,6 +4,7 @@ import com.epam.test_task_ihg.bean.Hotel;
 import com.epam.test_task_ihg.constants.Constants;
 import com.epam.test_task_ihg.store.exception.HotelsStoreException;
 import com.epam.test_task_ihg.util.HotelParserTxt;
+import com.epam.test_task_ihg.util.HotelsParserJson;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -25,7 +26,7 @@ public class HotelsStore {
                 prop.load(input);
 
 
-            hotelsList = HotelParserTxt.parse(new File(prop.getProperty(Constants.PROPERTY_NAME_PATH)));
+            hotelsList = HotelsParserJson.parse(prop.getProperty(Constants.PROPERTY_NAME_PATH_JSON));
         } catch (IOException e) {
             throw new HotelsStoreException();
         }
