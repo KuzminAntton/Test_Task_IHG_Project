d41a5448096f5997170a3d3cf4226d9c0a26e5f3
KuzminAntton <AntonAnton1488>
2019-04-17 14:19:31 +0300

    refactoring

--- src/main/java/com/epam/test_task_ihg/start/Main.java
+++ src/main/java/com/epam/test_task_ihg/start/Main.java
@@ -1,13 +1,12 @@
 package com.epam.test_task_ihg.start;
 
-import com.epam.test_task_ihg.controller.exception.ControllerException;
 import com.epam.test_task_ihg.view.ConsoleCommunication;
 
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ControllerException {
+    public static void main(String[] args) throws IOException {
         ConsoleCommunication consoleCommunication = new ConsoleCommunication();
 
         consoleCommunication.consoleCommunication();
