daa204c9950685d568f1fdec9c95bb45388c86ea
KuzminAntton <AntonAnton1488>
2019-04-23 14:56:09 +0300

    Controller exception

---
+++ src/main/groovy/src/controller/exception/ControllerException.groovy
@@ -0,0 +1,21 @@
+package src.controller.exception
+
+class ControllerException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public ControllerException() {
+        super();
+    }
+
+    public ControllerException(String message) {
+        super(message);
+    }
+
+    public ControllerException(Exception e) {
+        super(e);
+    }
+
+    public ControllerException(String message, Exception e) {
+        super(message, e);
+    }
+}
