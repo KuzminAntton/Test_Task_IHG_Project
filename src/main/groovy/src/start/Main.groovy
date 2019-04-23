a0e629c9b2a7e228115ecf3a9f6345f9f5868a01
KuzminAntton <AntonAnton1488>
2019-04-23 14:57:28 +0300

    console runner

---
+++ src/main/groovy/src/start/Main.groovy
@@ -0,0 +1,15 @@
+package src.start
+
+import src.view.impl.ConsoleCommunicationImpl
+
+
+class Main {
+     static void main(String[] args) {
+
+         ConsoleCommunicationImpl consoleCommunication = new ConsoleCommunicationImpl()
+
+         consoleCommunication.consoleCommunication()
+
+
+    }
+}
