ea47450b965b7420aa907c6a5574433fe0feacf3
KuzminAntton <AntonAnton1488>
2019-04-23 14:55:12 +0300

    enum for commands name

---
+++ src/main/groovy/src/controller/commands/CommandName.groovy
@@ -0,0 +1,10 @@
+package src.controller.commands
+
+enum CommandName {
+    GET_FIRST_FIVE_HOTELS_BY_RATE,
+    GET_HOTELS_BY_GROUP,
+    GET_HOTELS_BY_PRICE_RANGE,
+    WRONG_REQUEST,
+    WRONG_COMMAND,
+    EXIT
+}
