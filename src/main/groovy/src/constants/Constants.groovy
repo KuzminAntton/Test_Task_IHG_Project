7d8dfc7d04e1276b88addde3c6fd4e73dd7bd9d2
KuzminAntton <AntonAnton1488>
2019-04-23 14:54:39 +0300

    constants groovy

---
+++ src/main/groovy/src/constants/Constants.groovy
@@ -0,0 +1,22 @@
+package src.constants
+
+class Constants {
+    final static String PROPERTY_PATH = "src\\main\\resources\\hotelsList.properties"
+    final static String PROPERTY_NAME_PATH = "path"
+    final static String PROPERTY_NAME_PATH_JSON = "jsonPath"
+    final static String HOTELS_THAT_I_COULD_FOUND = "Hotels that I could found."
+    final static String ALL_GROUPS_THAT_I_COULD_FOUND = "All groups that I could found."
+    final static String HOTELS_THAT_I_COULD_FOUND_IN_THREE_PRICE_RANGES = "Hotels that I could found in three price ranges."
+    final static String BEST_REGARDS_AND_HAVE_I_NICE_DAY = "Best regrds and have I nice day."
+    final static String REQUEST_IS_NOT_CORRECT = "Request is not correct"
+    final static String COMMAND_IS_NOT_CORRECT = "Command is not correct"
+    final static String PARAM_DELIMETER = "-"
+    final static String EXIT_MESSAGE = "exit-"
+    final static String BREAKFAST_NOT_INCLUDED = "breakfast_not_included"
+    final static String BREAKFAST_INCLUDED = "breakfast_included"
+    final static String HOTEL = "Hotel"
+    final static String APARTMENT = "Apartment"
+    final static String HOSTEL = "Hostel"
+    final static String PATH_TOJSON = "src\\main\\resources\\hotels.json"
+    final static String CHARSET_UTF8 = "UTF-8"
+}
